7ef990e7718228f7e894472544b48307ff9fd0c9
Elena Donyakina <hellengoll@gmail.com>
2023-05-12 23:05:59 +0300

    new tests added to product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,15 +10,25 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
 
     def book_should_be_added(self):
-        assert self.is_element_present(*ProductPageLocators.BOOK_IS_ADDED), \
+        name_of_book_el = self.browser.find_element(*ProductPageLocators.NAME_OF_BOOK)
+        name_of_book = name_of_book_el.text
+        name_of_book_in_basket_el = self.browser.find_element(*ProductPageLocators.NAME_OF_BOOK_IN_BASKET)
+        name_of_book_in_basket = name_of_book_in_basket_el.text
+        assert name_of_book == name_of_book_in_basket, \
             'Book has not been added'
 
     def price_should_be_right(self):
         price_of_book_el = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK)
         price_of_book = price_of_book_el.text
         price_of_book_in_basket_el = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK_IN_BASKET)
-        price_of_book_in_basket_all = price_of_book_in_basket_el.text
-        length_price = len(price_of_book)
-        price_of_book_in_basket = price_of_book_in_basket_all[-length_price::]
+        price_of_book_in_basket = price_of_book_in_basket_el.text
         assert price_of_book == price_of_book_in_basket, \
             'Prices are not equal'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.NAME_OF_BOOK_IN_BASKET), \
+            'Success message is presented, but should not be'
+
+    def message_should_be_dissapeared(self):
+        assert self.is_disappeared(*ProductPageLocators.NAME_OF_BOOK_IN_BASKET), \
+            'Success message is presented, but should not be'
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,55 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+import pytest
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link1 = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link2 = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+link3 = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('number', ['0', '1', '2', '3', '4', '5', '6',
+                                    pytest.param('7', marks=pytest.mark.xfail),
+                                    '8', 9])
+def test_guest_can_add_product_to_basket(browser, number):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.book_should_be_added()
     page.price_should_be_right()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link1)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link1)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link1)
+    page.open()
+    page.add_product_to_basket()
+    page.message_should_be_dissapeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link3)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link3)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
