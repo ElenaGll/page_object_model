9478a0ad4cd1b0a9cde40ee536f9e6040b34374b
Elena Donyakina <hellengoll@gmail.com>
2023-05-12 23:03:38 +0300

    deleted main tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,4 @@
 from .base_page import BasePage
-from .locators import MainPageLocaators
 
 
 class MainPage(BasePage):
@@ -7,10 +6,5 @@ class MainPage(BasePage):
     The method is linked to the main page of the online store
     """
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocaators.LOGIN_LINK)
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocaators.LOGIN_LINK), \
-            'Login link is not presented'
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
