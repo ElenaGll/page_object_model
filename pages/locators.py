41827c82402cbcddab3220287db48d67f9974dd2
Elena Donyakina <hellengoll@gmail.com>
2023-05-14 22:32:15 +0300

    locators updated

--- pages/locators.py
+++ pages/locators.py
@@ -4,11 +4,21 @@ from selenium.webdriver.common.by import By
 class BasicPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
+    BASKET_LINK = (By.CSS_SELECTOR, 'div.row div.basket-mini span.btn-group a.btn-default')
+
+
+class BasketPageLocators():
+    TEXT_ABOUT_EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner p')
+    BASKET_TITLE = (By.CSS_SELECTOR, '#content_inner div.basket-title div.row h2')
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, 'div.login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, 'div.register_form')
+
+
+class MainPageLocators():
+    MAIN_LINK = (By.CSS_SELECTOR, 'div.page_inner ul.breadcrumb li:first-of-type')
 
 
 class ProductPageLocators():
