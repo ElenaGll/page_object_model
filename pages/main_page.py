9b0e24a542069ed1d7a38e2fdc9da8ef75a7ed41
Elena Donyakina <hellengoll@gmail.com>
2023-05-14 22:30:50 +0300

    main page/test updated

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
@@ -6,5 +7,6 @@ class MainPage(BasePage):
     The method is linked to the main page of the online store
     """
 
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+    def go_to_main_page(self):
+        main_link = self.browser.find_element(*MainPageLocators.MAIN_LINK)
+        main_link.click()
